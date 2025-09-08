25ca9af1488a75208b1a739f1894bb90c2799b4b
Heat2002 <haroldalvaradotineo@gmail.com>
2025-09-08 11:38:39 -0500

    fix

--- pages/cargaconsolidada/abiertos/cotizaciones/[id].vue
+++ pages/cargaconsolidada/abiertos/cotizaciones/[id].vue
@@ -895,7 +895,7 @@ const embarqueCotizadorColumns = ref<TableColumn<any>[]>([
                     h(UButton, {
                         icon: 'i-heroicons-eye',
                         variant: 'ghost',
-                        color: 'neutral',
+                        color: 'info',
                         size: 'md',
                         onClick: () => {
                             navigateTo(`/cargaconsolidada/abiertos/cotizaciones/proveedor/documentacion/${proveedor.id}`)
@@ -904,7 +904,7 @@ const embarqueCotizadorColumns = ref<TableColumn<any>[]>([
                     h(UButton, {
                         icon: 'material-symbols:save-sharp',
                         variant: 'ghost',
-                        color: 'neutral',
+                        color: 'primary',
                         size: 'md',
                         onClick: () => {
                             updateProveedorData(proveedor)
