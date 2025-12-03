d80cbd711d7dbb9c0fdeac9f6edfadfdc78d501c
HarukaDev <harukakasugano31@gmail.com>
2025-12-03 18:45:09 -0500

    fix

--- pages/cargaconsolidada/completados/cotizacion-final/[id].vue
+++ pages/cargaconsolidada/completados/cotizacion-final/[id].vue
@@ -615,7 +615,7 @@ const pagosColumns = ref<TableColumn<any>[]>([
     cell: ({ row }: { row: any }) => {
       let MAX_PAYMENTS = 4;
       //IF apagar > MAX_PAYMENTS and length(pagos) > MAX_PAYMENTS then MAX_PAYMENTS ++
-      if (row.original.total_logistica_impuestos > row.original.total_pagos && JSON.parse(row.original.pagos || '[]').length > MAX_PAYMENTS) {
+      if (row.original.total_logistica_impuestos > row.original.total_pagos && JSON.parse(row.original.pagos || '[]').length >= MAX_PAYMENTS) {
         MAX_PAYMENTS++; 
       }
       return !row.original.id_contenedor_pago ?
