b9537ee013fa156a1ebad8047d403bc54f8151d6
Haruka31Dev <harukakasugano31@gmail.com>
2026-01-29 10:10:08 -0500

    fix

--- config/websocket/events/reintegros.ts
+++ config/websocket/events/reintegros.ts
@@ -19,7 +19,12 @@ import {
     // Handler para CotizacionStatusUpdated
     registerEventHandler(WS_EVENTS.REINTEGRO_REQUEST, (data) => {
       const { showSuccess } = useModal()
-      showSuccess('Reintegro solicitado', data.message || 'Se ha solicitado el reintegro exitosamente.')
+      //data.data ="{\"viatico_id\":4,\"viatico_subject\":\"Taxi\",\"viatico_total_amount\":\"35.00\",\"viatico_status\":\"PENDING\",\"usuario_id\":28790,\"usuario_nombre\":\"Danitza\",\"message\":\"Vi\\u00e1tico creado exitosamente\",\"updated_at\":\"2026-01-29T15:07:51+00:00\"}"
+
+      const viatico = JSON.parse(data.data) 
+      const {usuario_nombre, viatico_subject, viatico_total_amount} = viatico
+      const message = `El usuario ${usuario_nombre} ha solicitado el reintegro de ${viatico_total_amount} para ${viatico_subject}.`
+      showSuccess('Reintegro solicitado', message)
     })
   
 
